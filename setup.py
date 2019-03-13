5d105e5678efdb9eb0f327a3e3e6854d5ef7c10c
bthornton191 <ben.thornton@mscsoftware.com>
2019-03-13 13:32:39 -0400

    added step_function function to the signal module

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="thornpy",
-    version="0.1.7",
+    version="0.1.8",
     author="Ben Thornton",
     author_email="ben.thornton@mscsoftware.com",
     description="Miscellaneous Python Tools",
