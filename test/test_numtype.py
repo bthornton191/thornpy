879e6fa7824700bcfa4d58867ae5d85877d05784
bthornton191 <ben.thornton@mscsoftware.com>
2019-04-15 18:12:45 -0400

    added str_is_num

--- test/test_numtype.py
+++ test/test_numtype.py
@@ -4,7 +4,11 @@ import unittest
 from thornpy import numtype
 
 FLOAT_STRING = '1.0'
+NEG_FLOAT_STRING = '-1.0'
 INT_STRING = '1'
+NEG_INT_STRING = '-1'
+CHAR_STRING = 'lskfsd'
+EMPTY_STRING = ''
 
 class Test_StrIsFloat(unittest.TestCase):
     """Tests that numtype.str_is_float() returns correct booleans
@@ -41,6 +45,66 @@ class Test_StrIsInt(unittest.TestCase):
         """Test that str_is_int() returns true when an int is passed
         """
         self.assertFalse(numtype.str_is_int(FLOAT_STRING))
+
+    def tearDown(self):
+        return
+
+class Test_StrIsPosNum(unittest.TestCase):
+    """Tests that numtype.str_is_pos_num() returns correct booleans
+    """
+
+    def setUp(self):
+        return
+
+    def test_int(self):
+        """Test that str_is_int() returns true when an int is passed
+        """
+        self.assertTrue(numtype.str_is_pos_num(INT_STRING))
+
+    def test_float(self):
+        """Test that str_is_int() returns true when a float is passed
+        """
+        self.assertTrue(numtype.str_is_pos_num(FLOAT_STRING))
+
+    def test_neg_int(self):
+        """Test that str_is_int() returns true when a negative int is passed
+        """
+        self.assertFalse(numtype.str_is_pos_num(NEG_INT_STRING))
+
+    def test_neg_float(self):
+        """Test that str_is_int() returns true when a negative float is passed
+        """
+        self.assertFalse(numtype.str_is_pos_num(NEG_FLOAT_STRING))
+
+    def tearDown(self):
+        return
+
+class Test_StrIsNum(unittest.TestCase):
+    """Tests that numtype.str_is_pos_num() returns correct booleans
+    """
+
+    def setUp(self):
+        return
+
+    def test_int(self):
+        """Test that str_is_int() returns true when an int is passed
+        """
+        self.assertTrue(numtype.str_is_num(INT_STRING))
+
+    def test_float(self):
+        """Test that str_is_int() returns true when a float is passed
+        """
+        self.assertTrue(numtype.str_is_num(FLOAT_STRING))
+
+    def test_neg_int(self):
+        """Test that str_is_int() returns true when a negative int is passed
+        """
+        self.assertTrue(numtype.str_is_num(NEG_INT_STRING))
+
+    def test_neg_float(self):
+        """Test that str_is_int() returns true when a negative float is passed
+        """
+        self.assertTrue(numtype.str_is_num(NEG_FLOAT_STRING))
 
     def tearDown(self):
         return
--- thornpy/numtype.py
+++ thornpy/numtype.py
@@ -1,25 +1,45 @@
 """numtype contains functions for checking the type of number that is contained within a string.
 """
 
-def str_is_pos_num(numeric_string):
-    """Returns true if the string is a number
+def str_is_num(string):
+    """Returns True if string is a number
     
-    Arguments
-    ---------
+    Parameters
+    ----------
     string : str
-        String representing a number
+        string possible representing a number
     
     Returns
     -------
     bool
-        True if the string is a float
+        True if string represents a number
+
     """
     is_numeric = False
-    if numeric_string:
-        # If the string is not empty check if it contains numbers
-        if numeric_string.replace('.','',1).replace('-','',1).isdigit():
-            is_numeric = True
+    if string:
+        if string.replace('.', '', 1).replace('-','',1).isdigit():
+            is_numeric = True    
+    return is_numeric
+
+def str_is_pos_num(string):
+    """Returns True if string is a positive number
     
+    Parameters
+    ----------
+    string : str
+        string possible representing a number
+    
+    Returns
+    -------
+    bool
+        True if string represents a positive number
+
+    """
+    is_numeric = False
+    if string:
+        # If the string is not empty check if it contains numbers
+        if string.replace('.','',1).isdigit():
+            is_numeric = True    
     return is_numeric
 
 
