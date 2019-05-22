c2080b68db327b47585f8e5d637ab4447f34845b
bthornton191 <ben.thornton@mscsoftware.com>
2019-05-22 14:27:26 -0500

    added utilities.read_data_string()

--- thornpy/utilities.py
+++ thornpy/utilities.py
@@ -39,3 +39,51 @@ def num_to_ith(num):
         suffix += ' to last'
 
     return  value + suffix
+
+def read_data_string(text, delimiter=',', newline='\n', has_headerline=True):
+    """Reads a delimited string into a list of dictionaries.  Functions very similar to :meth:`numpy.genfromtxt`, but for strings instead of text files.
+    
+    Parameters
+    ----------
+    text : str
+        String of row/column data with delimiters and new line indicators given in `delimiter` and `newline`.
+    delimiter : str, optional
+        Delimiter used in `text`, by default ','
+    newline : str, optional
+        New line indicator used in `text`, by default '\n'
+    has_headerline : bool, optional
+        If True, treats the first line of `text` as headers. If False, treats the first line of `text` as data and makes generic headers, by default True
+    
+    Returns
+    -------
+    :obj:`list` of :obj:`dict`
+        A list of dictionaries containing the data from `text`
+
+    """
+    lines = text.split(newline)
+    
+    # Generate headers
+    if has_headerline:
+        # If the text has headerlines, get them
+        headers = lines.pop(0).split(delimiter)
+
+    else:
+        # If the text doesn't have headerlines, make generic ones
+        headers = [str(i+1) for i in range(len(lines[0].split(delimiter)))]
+    
+    data = []
+    for line in lines:
+        # For each line, check if data is missing
+                
+        if len(line.split(delimiter)) == len(headers):
+            # If there is no missing data on this line, initialize a dictionary for the line data
+            line_data = {}       
+            
+            for header, value in zip(headers, line.split(delimiter)):
+                # For each column in the line, add to the line_data dict (header as key and value as value)
+                line_data[header] = value
+            
+            # Append the line_data dict to the data list
+            data.append(line_data)
+
+    return data
