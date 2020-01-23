897478b65b435fd24d6762e79ec2d0c983fc0440
Ben Thornton <ben.thornton@mscsoftware.com>
2020-01-23 11:30:05 -0500

    uploded to pip

--- upload_to_pip.bat
+++ upload_to_pip.bat
@@ -2,4 +2,4 @@ rmdir /S /Q dist
 rmdir /S /Q build
 rmdir /S /Q adamspy.egg-info
 python setup.py sdist bdist_wheel
-C:\Users\bthornt\AppData\Roaming\Python\Python36\Scripts\twine.exe upload dist/*
+twine upload dist/*
