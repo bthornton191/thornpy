0dec4aaf8747bf6f1baf63102fac28b756ec308e
Ben Thornton <ben.thornton@mscsoftware.com>
2020-11-30 10:56:23 -0500

    updated version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="thornpy",
-    version="0.6.0",
+    version="0.7.0",
     author="Ben Thornton",
     author_email="ben.thornton@mscsoftware.com",
     description="Miscellaneous Python Tools",
