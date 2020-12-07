07cc01294905e7bcf0f5923092d3892dc2d4879d
Ben Thornton <ben.thornton@mscsoftware.com>
2020-12-07 15:32:24 -0500

    uploading to pip

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="thornpy",
-    version="0.8.1",
+    version="0.8.2",
     author="Ben Thornton",
     author_email="ben.thornton@mscsoftware.com",
     description="Miscellaneous Python Tools",
